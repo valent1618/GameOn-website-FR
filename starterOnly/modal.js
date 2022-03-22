18f3ab76266838ac12575e0eec0dd0e0a6a21d9a
Valentin Lemaire <valentinlemaire1618@gmail.com>
2022-03-22 18:33:24 +0100

    handle validation message

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -2,8 +2,17 @@
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeModalBtn = document.querySelector(".close");
+const formBody = document.querySelector(".form-body");
+const form = document.querySelector("form");
 const input = document.querySelectorAll("input");
+const labelTournament = document.getElementById("tournament");
 const submit = document.getElementsByClassName("btn-submit")[0];
+
+// Media Mobile
+if (window.matchMedia("(max-width: 768px)").matches) {
+  labelTournament.innerText =
+    "À combien de tournois avez-vous déjà participé ?";
+}
 
 /*** --- Events --- ***/
 // launch modal event
@@ -17,22 +26,47 @@ input.forEach((ipt) =>
   ipt.addEventListener(
     "input",
     () => {
-      // Chaque fois que l'utilisateur saisit quelque chose
-      // on vérifie la validité.
+      // Each time user types somtehing
+      // we check the validity and display error messages
       handleError(ipt);
+      // and we reset animation on submit button
+      submit.style.animation = "";
     },
     false
   )
 );
 
-// Detect validity form on
+// Detect validity form on submit
 submit.addEventListener(
   "click",
   (e) => {
-    // Chaque fois que l'utilisateur tente d'envoyer les données
-    // on vérifie que les champs sont valides.
-    input.forEach((ipt) => handleError(ipt));
     e.preventDefault();
+    // if submit button value is "C'est parti"
+    if (submit.value === "C'est parti") {
+      // and if the form is not valide
+      if (form.checkValidity() === false) {
+        // we display error messages
+        input.forEach((ipt) => handleError(ipt));
+        // and we add animation on submit button
+        submit.style.animation =
+          "shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both";
+      } else {
+        // else if we save the name
+        let name = document.getElementById("first").value;
+        // we change HTML of the formBody
+        // for a message with the name
+        formBody.innerHTML = `<h2 class='validation'>Merci pour votre inscription ${name}</h2>`;
+        formBody.className = "form-body form-body-validation";
+        // we keep the background of submit in red
+        submit.style.background = "var(--color-primary)";
+        // and we change it's value for "Fermer"
+        submit.value = "Fermer";
+      }
+    } else {
+      // if submit button value is "Fermer"
+      // we close the modal
+      closeModal();
+    }
   },
   false
 );
